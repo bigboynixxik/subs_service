4ff609422097b6a40972d249121040b3f2b18d56
Nixxik <v.gorbachev05@yandex.ru>
2026-04-30 17:12:27 +0700

    fix: model subscription

--- internal/models/subscription.go
+++ internal/models/subscription.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Subscription struct {
-	ID          uuid.UUID  `json:"id" db:"id"`
-	ServiceName string     `json:"service_name" db:"service_name"`
-	Price       int        `json:"price" db:"price"`
-	UserID      uuid.UUID  `json:"user_id" db:"user_id"`
-	StartDate   time.Time  `json:"start_date" db:"start_date"`
-	EndDate     *time.Time `json:"end_date,omitempty" db:"end_date"`
+	ID          uuid.UUID  `json:"id"`
+	ServiceName string     `json:"service_name"`
+	Price       int        `json:"price"`
+	UserID      uuid.UUID  `json:"user_id"`
+	StartDate   time.Time  `json:"start_date"`
+	EndDate     *time.Time `json:"end_date,omitempty"`
 }
